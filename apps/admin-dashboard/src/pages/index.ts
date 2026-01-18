28541134fc4bf3c7aa7a5e8a71fa945eed307871
Patrick Deutsch <105461352+kcirtapfromspace@users.noreply.github.com>
2026-01-18 16:22:36 -0700

    feat: US-031 - Create Admin Dashboard API client

---
+++ apps/admin-dashboard/src/pages/index.ts
@@ -0,0 +1,7 @@
+export { LoginPage } from './LoginPage';
+export { DashboardPage } from './DashboardPage';
+export { WalkersPage } from './WalkersPage';
+export { ServicesPage } from './ServicesPage';
+export { BookingsPage } from './BookingsPage';
+export { CustomersPage } from './CustomersPage';
+export { SettingsPage } from './SettingsPage';
